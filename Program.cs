754f435b53c9aac1dc6d82973cf90e32b312aafb
root <root@DESKTOP-QRMR1UQ.localdomain>
2020-02-26 00:26:15 -0400

    Lista

--- Program.cs
+++ Program.cs
@@ -8,114 +8,40 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            string aulaIntro = "Introdução às Coleções";
-            string aulaModelando = "Modelando a Classe Aula";
-            string aulaSets = "Trabalhando com conjuntos";
+            var aulaIntro = new Aula("Introdução às Coleções", 20);
+            var aulaModelando = new Aula("Modelando a Classe Aula", 18);
+            var aulaSets = new Aula("Trabalhando com conjuntos", 10);
 
-            //List<string> aulas = new List<string>
-            //{
-            //    aulaIntro,
-            //    aulaModelando,
-            //    aulaSets
-            //};
-
-            List<string> aulas = new List<string>();
+            List<Aula> aulas = new List<Aula>();
             aulas.Add(aulaIntro);
             aulas.Add(aulaModelando);
             aulas.Add(aulaSets);
 
-            //Console.WriteLine("A primeira aula é: " + aulas.First());
-            //Console.WriteLine("A última aula é: " + aulas.Last());
-
-            aulas[0] = "Trabalhando com Listas";
             Imprimir(aulas);
-
-            Console.WriteLine("A primeira aula 'Trabalhando' é: " + aulas.First(aula => aula.Contains("Trabalhando")));
-
-            Console.WriteLine("A úlima aula 'Trabalhando' é: " + aulas.Last(aula => aula.Contains("Trabalhando")));
-
-            Console.WriteLine("A primeira aula 'Conclusão' é: " + aulas.FirstOrDefault(aula => aula.Contains("Conclusão")));
-
-            aulas.Reverse();
-
-            Imprimir(aulas);
-
-
-            aulas.RemoveAt(aulas.Count - 1);
-
-            aulas.Add("Conclusão");
-            
-            aulas.Sort();
-            Imprimir(aulas);
-
-            List<string> copia = aulas.GetRange(aulas.Count() - 2, 2); 
-            Imprimir(copia);
-
-            List<string> clone = new List<string>(aulas);
-            Imprimir(copia);
-
-            clone.RemoveRange(clone.Count - 2, 2);
-            Imprimir(clone);
-
-            #region Aula Arrays
-            //string[] aulas = new string[] 
-            //{
-            //    aulaIntro,
-            //    aulaModelando,
-            //    aulaSets
-            //};
-
-            //string[] aulas = new string[3];
-            //aulas[0] = aulaIntro;
-            //aulas[1] = aulaModelando;
-            //aulas[2] = aulaSets;
-
-            //Imprimir(aulas);
-
-            //aulaIntro = "TrabalhandoComArrays";
-
-            //aulas[0] = "Gushtaaaavo";
-
-            //Console.WriteLine(aulas[0]);
-            //Console.WriteLine(aulas[aulas.Length - 1]);
-
-            //Console.WriteLine("Aula modelando está no indice " + Array.IndexOf(aulas, aulaModelando));
-            //Console.WriteLine(Array.LastIndexOf(aulas, aulaModelando));
-
-            //Array.Reverse(aulas);
-
-            //Array.Resize(ref aulas, 2);
-            //Imprimir(aulas);
-
-            //Array.Sort(aulas);
-            //Imprimir(aulas);
-
-            //string[] copia = new string[2];
-
-            //Array.Copy(aulas, 0, copia, 0, 2);
-            //Imprimir(copia);
-
-            //string[] clone = aulas.Clone() as string[];
-            //Imprimir(clone);
-
-            //Array.Clear(clone, 1, 2);
-            //Imprimir(clone);
-
-            #endregion
         }
 
-        public static void Imprimir(List<string> aulas)
+        private static void Imprimir(List<Aula> aulas)
         {
-            Console.Write("\n");
-            aulas.ForEach(aula =>
+            Console.Clear();
+            foreach (var aula in aulas)
             {
                 Console.WriteLine(aula);
-            });
-
-            //for (int i = 0; i < aulas.Length; i++)
-            //{
-            //    Console.WriteLine(aulas[i]);
-            //}
+            }
         }
+    }
+
+    class Aula
+    {
+        string _aula;
+        int _hora;
+
+        public Aula(string aula, int hora)
+        {
+            _aula = aula;
+            _hora = hora;
+        }
+
+        public string Aulas { get => _aula; set => _aula = value; }
+        public int Hora { get => _hora; set => _hora = value; }
     }
 }
